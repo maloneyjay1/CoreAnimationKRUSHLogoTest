3a2f5cf5dc28f24e06deee30e272e46247c18a87
Jay Maloney <maloneyjay1@gmail.com>
2016-04-19 12:09:45 -0400

    Initial Commit

---
+++ customCoreAnimationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  customCoreAnimationTest
+//
+//  Created by Jay Maloney on 4/19/16.
+//  Copyright © 2016 Jay Maloney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
